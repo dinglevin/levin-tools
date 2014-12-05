cd216410d42effe70934fabd5f74855c52c8e493
dinglevin <dingweiqiong@gmail.com>
2014-12-06 00:21:10 +0800

    Initial import for ReferenceCountSet class

---
+++ src/main/java/org/levin/tools/corejava/sets/ReferenceCountSet.java
@@ -0,0 +1,81 @@
+package org.levin.tools.corejava.sets;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ReferenceCountSet<E> extends AbstractSet<E> {
+    private volatile ConcurrentMap<E, AtomicInteger> data;
+    
+    public ReferenceCountSet() {
+        initData();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+    
+    public int getRefCount(E element) {
+        // This is thread safe as data is a ConcurrentMap which is thread safe
+        AtomicInteger ref = data.get(element);
+        return ref == null ? 0 : ref.get();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object obj) {
+        return data.containsKey(obj);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return data.keySet().iterator();
+    }
+
+    @Override
+    public boolean add(E element) {
+        AtomicInteger refCount = data.get(element);
+        if (refCount == null) {
+            // Avoid to put AtomicInteger(0) as during remove we need this value to compare
+            AtomicInteger newRefCount = new AtomicInteger(1);
+            refCount = data.putIfAbsent(element, newRefCount);
+            if (refCount == null) {
+                return true;
+            }
+        }
+        refCount.incrementAndGet();
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        AtomicInteger refCount = data.get(obj);
+        if (refCount == null) {
+            return false;
+        }
+        if (refCount.decrementAndGet() <= 0) {
+            return data.remove(obj, refCount);
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // Instead of call it's clear method, I simply discard the previous map 
+        // and create a new one, then the old one will be garbage collected.
+        // This way is we can make it thread safe with copy-on-write mechanism, 
+        // and is faster than the clear method especially when the items a large in the previous map
+        initData();
+    }
+    
+    private void initData() {
+        data = new ConcurrentHashMap<E, AtomicInteger>();
+    }
+}
---
+++ src/main/java/org/levin/tools/test/MultiThreadTester.java
@@ -0,0 +1,57 @@
+package org.levin.tools.test;
+
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultiThreadTester {
+    private final long sleepBefore;
+    private final int threadCount;
+    private final ExecutorService executor;
+    
+    public MultiThreadTester(long sleepBefore, int threadCount) {
+        this.sleepBefore = sleepBefore;
+        this.threadCount = threadCount;
+        this.executor = Executors.newFixedThreadPool(threadCount);
+    }
+    
+    public void execute(TestTask testTask) throws Exception {
+        CompletionService<Void> completionService = new ExecutorCompletionService<Void>(executor);
+        for (int i = 0; i < threadCount; i++) {
+            completionService.submit(new InnerRunnable(testTask, i), null);
+        }
+        for (int i = 0; i < threadCount; i++) {
+            completionService.take().get();
+        }
+    }
+    
+    public static interface TestTask {
+        public void run(int threadIndex);
+    }
+    
+    private class InnerRunnable implements Runnable {
+        private final TestTask proxy;
+        private final int threadIndex;
+        
+        public InnerRunnable(TestTask proxy, int threadIndex) {
+            this.proxy = proxy;
+            this.threadIndex = threadIndex;
+        }
+        
+        public void run() {
+            if (proxy != null) {
+                if (sleepBefore > 0) {
+                    try {
+                        Thread.sleep(sleepBefore);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                proxy.run(threadIndex);
+            } else {
+                throw new IllegalStateException("proxy is null");
+            }
+        }
+    }
+}
